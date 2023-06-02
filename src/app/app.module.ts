2e5f5b05edf6d04038d46fa0b82b2c2f104c4463
Trystin Flanigan <Flanigan.trystin@gmail.com>
2023-06-02 10:46:36 -0600

    Adapting basic interface and styling into Angular. Delegated interface to a component to learn how they work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterfaceComponent } from './interface/interface.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterfaceComponent
   ],
   imports: [
     BrowserModule,
